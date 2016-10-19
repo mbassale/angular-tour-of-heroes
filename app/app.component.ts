30eafb08a1b3c935119d1d26af16fb29401af8fb
Marco Bassaletti <bassaletti@gmail.com>
2016-10-19 02:54:02 -0300

    Initial Commit: Angular Tour Of Heroes Tutorial.

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by mbassale on 19-10-16.
+ */
+import { Component } from '@angular/core';
+@Component({
+    selector: 'my-app',
+    template: '<h1>Angular Route Of Heroes</h1>'
+})
+export class AppComponent { }
---
+++ app/main.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by mbassale on 19-10-16.
+ */
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
