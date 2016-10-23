7e0b081e54606dcc0d6c300f7ca1b41c8c4b7e6b
Marco Bassaletti <bassaletti@gmail.com>
2016-10-23 00:55:20 -0300

    Created service to get heroes using mock implementation and promises for asynchronous behaviour.

---
+++ app/hero.service.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by mbassale on 23-10-16.
+ */
+
+import {Injectable} from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+@Injectable()
+export class HeroService {
+    getHeroes(): Promise<Hero[]> {
+        return Promise.resolve(HEROES);
+    }
+}
