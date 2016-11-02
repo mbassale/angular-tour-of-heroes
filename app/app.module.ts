09b2f8b46bdc1d34237f39f0203dee77f445a3ae
Marco Bassaletti <bassaletti@gmail.com>
2016-11-02 01:25:25 -0300

    Refactored into components and added router for easier navigation between components.

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,19 +1,31 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 
-import { AppComponent }   from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
+import {AppComponent} from './app.component';
+import {HeroService} from './hero.service';
+import {HeroesComponent} from './heroes.component';
+import {HeroDetailComponent} from './hero-detail.component';
+import {DashboardComponent} from "./dashboard.component";
+import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
     imports: [
         BrowserModule,
-        FormsModule
+        FormsModule,
+        AppRoutingModule
     ],
     declarations: [
         AppComponent,
+        DashboardComponent,
+        HeroesComponent,
         HeroDetailComponent
     ],
-    bootstrap:    [ AppComponent ]
+    providers: [
+        HeroService
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
---
+++ app/heroes.component.ts
@@ -0,0 +1,40 @@
+/**
+ * Created by mbassale on 19-10-16.
+ */
+import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+import {Router} from "@angular/router";
+
+@Component({
+    moduleId: module.id,
+    selector: 'my-heroes',
+    templateUrl: 'heroes.component.html',
+    styleUrls: ['heroes.component.css'],
+    providers: [HeroService]
+})
+export class HeroesComponent implements OnInit {
+    title = 'Tour of Heroes';
+    heroes: Hero[];
+    selectedHero: Hero;
+
+    constructor(private router: Router, private heroService: HeroService) {
+    }
+
+    ngOnInit(): void {
+        this.getHeroes();
+    }
+
+    getHeroes(): void {
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
+
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    }
+
+    gotoDetail(): void {
+        this.router.navigate(['/detail', this.selectedHero.id]);
+    }
+}
